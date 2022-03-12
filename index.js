99a0512b35a36438acb0e6255bc487b1084e3472
Yoann <ybuzenet@gmail.com>
2022-03-12 20:41:55 +0100

    first index + logger

---
+++ index.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const { logger } = require("./logger");
+
+const fastify = require("fastify")({ logger: { prettyPrint: true } });
+
+fastify.register(require("fastify-cors"), {
+  origin: true,
+  // https://github.com/fastify/fastify-cors
+});
+
+fastify.register(require("./routes/web_structure/webStructure.js"), {
+  prefix: "/api/webStructure/websiteDisplay",
+});
+
+// Loading debug env variables
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+  const result = require("dotenv").config({
+    path: path.resolve(process.cwd(), "./.env.local"),
+  });
+  if (result.error) {
+    throw (
+      ".env.local could not be found for non production environment loading. Did you create it ?" +
+      result.error
+    );
+  } else {
+    console.log("SUCCESS - .env.local file found.");
+  }
+}
+// Refacto in Development module to load ?
+// Loading debug routes
+if (process.env.NODE_ENV !== "production") {
+  console.log("Loading debug routes into the app");
+  fastify.register(require("./routes/debug_routes/test_DB.js"), {
+    prefix: "/api/test/DB",
+  });
+}
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(3001, "0.0.0.0");
+  } catch (err) {
+    console.log("err", err);
+    logger.log("error", `FASTIFY ERROR - ${err}"`);
+
+    process.exit(1);
+  }
+};
+
+start();
---
+++ logger/index.js
@@ -0,0 +1,33 @@
+const { createLogger, format, transports } = require("winston");
+const { timestamp, combine, printf } = format;
+
+const myFormat = printf(({ level, message, timestamp }) => {
+  return `${timestamp} ${level}: ${message}`;
+});
+
+const logger = createLogger({
+  format: combine(timestamp(), myFormat),
+  defaultMeta: { service: "user-service" },
+  transports: [
+    //
+    // - Write all logs with level `error` and below to `error.log`
+    // - Write all logs with level `info` and below to `combined.log`
+    //
+    new transports.File({
+      filename: "logs/error.log",
+      level: "error",
+      maxSize: 10000000,
+      maxFiles: 1,
+    }),
+    new transports.File({
+      filename: "logs/combined.log",
+      maxSize: 10000000,
+      maxFiles: 1,
+    }),
+  ],
+  exitOnError: false,
+});
+
+module.exports = {
+  logger,
+};
