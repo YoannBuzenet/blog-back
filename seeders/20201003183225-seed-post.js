2c2a6b7df1a749687ffa12e8cbeedf443a3caca0
Yoann Buzenet <ybuzenet@gmail.com>
2022-07-03 13:49:30 +0200

    creation de 12 seeds de post pour mieux tester

--- seeders/20201003183225-seed-post.js
+++ seeders/20201003183225-seed-post.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const { formatSimple, formatComplex } = require("../services/react-slate");
+const { sampleContent, sampleMetaDescription } = require("./dataSample/posts");
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
@@ -8,18 +9,120 @@ module.exports = {
       "Posts",
       [
         {
-          title: formatSimple(
-            "Vous pouvez nous contacter chaque jour de la semaine par téléphone et email."
-          ),
-          shortDescription: formatSimple(
-            "Vous pouvez nous contacter chaque jour de la semaine par téléphone et email."
-          ),
-          content: formatSimple(
-            "Vous pouvez nous contacter chaque jour de la semaine par téléphone et email."
-          ),
-          metaDescription: formatSimple(
-            "Vous pouvez nous contacter chaque jour de la semaine par téléphone et email."
-          ),
+          title: `[{"type":"paragraph","children":[{"text":"Le plus grand ennemi de Tesla"}]}]`,
+          shortDescription: sampleMetaDescription,
+          content: sampleContent,
+          metaDescription: sampleMetaDescription,
+          isScoop: true,
+          UserId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: `[{"type":"paragraph","children":[{"text":"La nouvelle librairie JS"}]}]`,
+          shortDescription: sampleMetaDescription,
+          content: sampleContent,
+          metaDescription: sampleMetaDescription,
+          isScoop: true,
+          UserId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: `[{"type":"paragraph","children":[{"text":"Lex Friedman reçoit Demis hassabis"}]}]`,
+          shortDescription: sampleMetaDescription,
+          content: sampleContent,
+          metaDescription: sampleMetaDescription,
+          isScoop: true,
+          UserId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: `[{"type":"paragraph","children":[{"text":"Aux confins de l'univers"}]}]`,
+          shortDescription: sampleMetaDescription,
+          content: sampleContent,
+          metaDescription: sampleMetaDescription,
+          isScoop: true,
+          UserId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: `[{"type":"paragraph","children":[{"text":"Mes 5 livres préférés"}]}]`,
+          shortDescription: sampleMetaDescription,
+          content: sampleContent,
+          metaDescription: sampleMetaDescription,
+          isScoop: true,
+          UserId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: `[{"type":"paragraph","children":[{"text":"Sociologie et déterminisme"}]}]`,
+          shortDescription: sampleMetaDescription,
+          content: sampleContent,
+          metaDescription: sampleMetaDescription,
+          isScoop: true,
+          UserId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: `[{"type":"paragraph","children":[{"text":"Comment penser l'IA ?"}]}]`,
+          shortDescription: sampleMetaDescription,
+          content: sampleContent,
+          metaDescription: sampleMetaDescription,
+          isScoop: true,
+          UserId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: `[{"type":"paragraph","children":[{"text":"Ce titre a été généré par une IA."}]}]`,
+          shortDescription: sampleMetaDescription,
+          content: sampleContent,
+          metaDescription: sampleMetaDescription,
+          isScoop: true,
+          UserId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: `[{"type":"paragraph","children":[{"text":"J'ai installé mon serveur !"}]}]`,
+          shortDescription: sampleMetaDescription,
+          content: sampleContent,
+          metaDescription: sampleMetaDescription,
+          isScoop: true,
+          UserId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: `[{"type":"paragraph","children":[{"text":"Linux, pour le meilleur et pour le reste"}]}]`,
+          shortDescription: sampleMetaDescription,
+          content: sampleContent,
+          metaDescription: sampleMetaDescription,
+          isScoop: true,
+          UserId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: `[{"type":"paragraph","children":[{"text":"Fin des travaux"}]}]`,
+          shortDescription: sampleMetaDescription,
+          content: sampleContent,
+          metaDescription: sampleMetaDescription,
+          isScoop: true,
+          UserId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: `[{"type":"paragraph","children":[{"text":"Emménagement !"}]}]`,
+          shortDescription: sampleMetaDescription,
+          content: sampleContent,
+          metaDescription: sampleMetaDescription,
           isScoop: true,
           UserId: 1,
           createdAt: new Date(),
