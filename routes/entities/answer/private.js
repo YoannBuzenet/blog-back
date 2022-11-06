434ed06fe2be87908039735908d2ba4d647a1fc9
Yoann <ybuzenet@gmail.com>
2022-11-06 17:06:03 +0100

    working on rearranging endpoints to better use hooks

---
+++ routes/entities/answer/private.js
@@ -0,0 +1,83 @@
+const { logger } = require("../../logger");
+const db = require("../../models/index");
+
+// Private endpoints for next blog-front only.
+
+module.exports = function (fastify, opts, done) {
+  fastify.get(
+    "/",
+    {
+      schema: {},
+    },
+    async (req, reply) => {
+      const isRequestAuthorized = isComingFromBlog(req.headers);
+
+      if (!isRequestAuthorized) {
+        reply.code(401).send("Unauthorized");
+        return;
+      }
+
+      try {
+        const { sort, limit } = req.query;
+
+        // On récupère toutes les propriétés du modèle pour filtrer les éventuels filtres reçus en query param
+        const allPropertiesFromAnswer = Object.keys(db.Answer.rawAttributes);
+
+        // Préparer la requete
+        let filters = {};
+
+        if (allPropertiesFromAnswer.includes(sort)) {
+          filters.order = [[sort, "DESC"]];
+        }
+        if (limit && +limit < MAX_PAGINATION) {
+          filters.limit = +limit;
+        } else {
+          filters.limit = MAX_PAGINATION;
+        }
+
+        const answer = await db.Answer.findAll(filters);
+
+        reply.code(200).send(answer);
+      } catch (error) {
+        logger.log("error", "Error while searching for all Answers :" + error);
+        reply.code(500).send(error);
+      }
+      return;
+    }
+  );
+
+  fastify.get(
+    "/:id",
+    {
+      schema: {},
+    },
+    async (req, reply) => {
+      const isRequestAuthorized = isComingFromBlog(req.headers);
+
+      if (!isRequestAuthorized) {
+        reply.code(401).send("Unauthorized");
+        return;
+      }
+
+      try {
+        const answer = await db.Answer.findOne({
+          where: {
+            id: req.params.id,
+          },
+        });
+        if (answer) {
+          reply.code(200).send(answer);
+        } else {
+          reply.code(404).send("answer non trouvé.");
+        }
+      } catch (error) {
+        logger.log("error", "Error while searching for all Answers :" + error);
+        reply.code(500).send(error);
+      }
+
+      return;
+    }
+  );
+
+  done();
+};
---
+++ routes/entities/answer/public.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const { logger } = require("../../logger");
+const db = require("../../models/index");
+const { belongsToRelevantUser } = require("../../services/authControl");
+const user = require("./user");
+
+module.exports = function (fastify, opts, done) {
+  // All answers for one post
+  fastify.get(
+    "/posts/:id",
+    {
+      schema: {},
+    },
+    async (req, reply) => {
+      try {
+        const answer = await db.Answer.findAll({
+          where: {
+            PostId: req.params.id,
+          },
+          include: {
+            model: db.User,
+            attributes: ["id", "nickname"],
+          },
+        });
+
+        // Removing all props but the one we want from user objects
+
+        if (answer) {
+          reply.code(200).send(answer);
+        } else {
+          reply.code(404).send("answers non trouvées.");
+        }
+      } catch (error) {
+        logger.log(
+          "error",
+          "Error while searching for all Answers for one post:" + error
+        );
+        reply.code(500).send(error);
+      }
+
+      return;
+    }
+  );
+
+  done();
+};
