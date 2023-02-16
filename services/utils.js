59f18f5acae64a7a69473b8dd39e1c520c839f73
Yoann <ybuzenet@gmail.com>
2023-02-16 22:32:02 +0100

    improving slugify function

---
+++ services/utils.js
@@ -0,0 +1,16 @@
+// https://gist.github.com/codeguy/6684588
+const slugify = (...args) => {
+    const value = args.join(' ')
+  
+    return value
+        .normalize('NFD') // split an accented letter in the base letter and the acent
+        .replace(/[\u0300-\u036f]/g, '') // remove all previously split accents
+        .toLowerCase()
+        .trim()
+        .replace(/[^a-z0-9 ]/g, '') // remove all chars not letters, numbers and spaces (to be replaced)
+        .replace(/\s+/g, '-') // separator
+  }
+    
+module.exports = {
+    slugify
+}
